6c39bc60d949a5188d9aa53d36df05f9bd212d5c
Sanskar gupta <Sanskar362002@gmail.com>
2025-07-23 23:19:29 +0530

    FIX :-Adding Answer section

--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	header := [12]byte{0x04, 0xD2, 0x80, 0, 0, 1, 0, 0, 0, 0, 0, 0}
+	header := [12]byte{0x04, 0xD2, 0x80, 0, 0, 1, 0, 1, 0, 0, 0, 0}
 	question := []byte("\x0ccodecrafters\x02io\x00\x00\x01\x00\x01")
 	answer := []byte("\x0ccodecrafters\x02io\x00\x00\x01\x00\x01\x00\x00\x00\x3C\x00\x04\x08\x08\x08\x08")
 	// Uncomment this block to pass the first stage
